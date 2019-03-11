c47a070026210c110203267a32be69e40ead1e91
Alex <amarvick94@gmail.com>
2019-03-11 16:02:15 -0700

    Adding fs to emailForwarding.js

--- email/actions/emailForwarding.js
+++ email/actions/emailForwarding.js
@@ -1,6 +1,7 @@
 const nodemailer = require('nodemailer')
 const express = require('express')
 const app = express()
+const fs = require('fs')
 const router = express.Router()
 
 const createTransport = require('./createTransport')
